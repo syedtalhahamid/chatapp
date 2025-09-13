78c71d5b01335c04ff877cba923cff88e4c1726b
syedtalhahamid <syedtalhacodenera@gmail.com>
2025-09-13 14:33:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,14 +3,12 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading")
 
-# Route for frontend
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Handle chat messages with username
 @socketio.on('send_message')
 def handle_message(data):
     username = data['username']
